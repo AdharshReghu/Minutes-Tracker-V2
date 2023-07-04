4bd4a1133a40c038adc677069b66f4741bea6f00
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-07-05 02:48:50 +0530

    UI of starting screen finished

---
+++ lib/views/starting.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( Starting());
+}
+
+class Starting  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Container(
+      color: Color(0x906366FF),
+      width: MediaQuery.of(context).size.height,
+      height:  MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(30),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset('./assets/StartingScreen.png'),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              width: double.infinity,
+                child: Text('Welcome to MeetMaster, your ultimate companion for mastering meetings like a pro',style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.w500),textAlign: TextAlign.center,),
+
+             // child: Text("Welcome to MeetMaster, your ultimate companion for mastering meetings like a pro"),
+            ),
+            SizedBox(height: 30,),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10), // Set the border radius value
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white), // Set the background color
+                  fixedSize: MaterialStateProperty.all<Size>(Size(double.infinity, 50)), // Set the size
+                ),
+                onPressed: () {
+                  // Button press action
+                },
+                child: Text("Get Started",style: TextStyle(fontSize: 20,color: Color(0xFF6366FF),fontWeight: FontWeight.w600)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),),);
+  }
+}
