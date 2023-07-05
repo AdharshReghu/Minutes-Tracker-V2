8336450b42695725fd637fb53e65485a88b33ac2
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-07-05 13:13:45 +0530

    login ui finished

---
+++ lib/constants/constants.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Color kMaintheme = Color(0x906366FF);
+
+InputDecoration kTextFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(20),
+    ),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: kMaintheme, width: 1),
+    borderRadius: BorderRadius.all(
+      Radius.circular(20),
+    ),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: kMaintheme, width: 2),
+    borderRadius: BorderRadius.all(
+      Radius.circular(20),
+    ),
+  ),
+);
+
+
+const TextStyle kGreyTextStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff878080),
+);
---
+++ lib/views/login.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:minutes_tracker/constants/constants.dart';
+
+
+void main(){
+  runApp( Login());
+}
+
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home:SafeArea(
+      child: Scaffold(body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            height: double.infinity,
+            child:  Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Welcome Back !",style: TextStyle(fontSize:30,fontWeight: FontWeight.w600),),
+                  const SizedBox(height: 30,),
+                  Image.asset("./assets/login.png"),
+                  SizedBox(height: 30,),
+                  Align(child: Text("Email",style: kGreyTextStyle,),alignment: Alignment.centerLeft,),
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    decoration: kTextFieldDecoration.copyWith(hintText: "Enter Your Email")
+                  ),
+                  SizedBox(height: 25,),
+                  Align(child: Text("Password",style: kGreyTextStyle,),alignment: Alignment.centerLeft,),
+                  SizedBox(height: 10,),
+                  TextFormField(
+                      decoration: kTextFieldDecoration.copyWith(hintText: "Enter Your Password")
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10), // Set the border radius value
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(kMaintheme), // Set the background color
+                        fixedSize: MaterialStateProperty.all<Size>(Size(double.infinity, 50)), // Set the size
+                      ),
+                      onPressed: () {
+                        // Button press action
+                      },
+                      child: Text("Get Started",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.w600)),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Text("New User?",style: kGreyTextStyle,),
+                  InkWell(child: Text("Register",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),onTap: (){},splashColor: kMaintheme,radius: 10,)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),),
+    ),);
+  }
+}
